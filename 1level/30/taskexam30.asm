b574a1a72791bcbdef6235d8396422d8181111de
Alexey <delvinru@gmail.com>
2021-01-25 03:41:17 +0300

    Some refactoring for best reading experience

--- 1level/30/taskexam30.asm
+++ 1level/30/taskexam30.asm
@@ -1,70 +1,43 @@
 section .data
-
-A dw 333
-
-a db 5
-
-b db 4
-
-C dw 444
+    A dw 333
+    a db 5
+    b db 4
+    C dw 444
 
 section .bss
-
-R resb 2
+    R resb 2
 
 section .text
-
-global _start
+    global _start
 
 _start:
+    mov bl, [a]
+    mov cx, [C]
+    mov bh, [b]
+    mov ax, [A]
 
-mov bl, [a]
-mov cx, [C]
-mov bh, [b]
-mov ax, [A]
+    call procedure
 
-call procedure
-
-mov eax,1
-mov ebx,0
-int 0x80
+    mov eax,1
+    mov ebx,0
+    int 0x80
 
 procedure:
+    push ebx; этот пуш для того, чтобы сохранять все используемые регистры
+    push ecx; тоже самое 
+    push ebx; этот пуш для того, чтобы сохранить bl
 
-push ebx; этот пуш для того, чтобы сохранять все используемые регистры
-push ecx; тоже самое 
+    mov ah,0; взять по модулю 256
+    mov bh,0; мы убираем лишнее из 16-разрядного операнда 'б' маленькое
+    sub ax,bx
 
-push ebx; этот пуш для того, чтобы сохранить bl
+    pop ebx
+    mov ch,0; мы занулили старшую часть регистра cx. привели по модулю 256. cx по прежнему 16 разрядный
+    mov bl,bh; переместили значение 'б' в младшую часть
+    mov bh,0; занулили копию
+    sub bx,cx; посчитали 2-ую скобку
+    add ax,bx
 
-mov ah,0; взять по модулю 256
-
-mov bh,0; мы убираем лишнее из 16-разрядного операнда 'б' маленькое
-
-sub ax,bx
- 
-
-
-pop ebx
-
-mov ch,0; мы занулили старшую часть регистра cx. привели по модулю 256. cx по прежнему 16 разрядный
-
-mov bl,bh; переместили значение 'б' в младшую часть
-
-mov bh,0; занулили копию
-
-
-sub bx,cx; посчитали 2-ую скобку
-
-add ax,bx
-
-pop ecx
-pop ebx
-
-ret
-
-
-
-
-
-
-
+    pop ecx
+    pop ebx
+    ret
--- 1level/31/taskexam31.asm
+++ 1level/31/taskexam31.asm
@@ -1,78 +1,50 @@
 section .data
-
-C dw 333
-
-a db 5
-
-c db 4
-
-d db 6
-
-D dw 444
+    C dw 333
+    a db 5
+    c db 4
+    d db 6
+    D dw 444
 
 section .bss
-
-R resb 2
+    R resb 2
 
 section .text
-
-global _start
+    global _start
 
 _start:
+    mov al, [a]
+    mov cx, [C]
+    mov bl, [c]
+    mov dx,[D]
+    mov bh,[d]
 
-mov al, [a]
-mov cx, [C]
-mov bl, [c]
-mov dx,[D]
-mov bh,[d]
+    call procedure
 
-call procedure
-
-mov eax,1
-mov ebx,0
-int 0x80
+    mov eax,1
+    mov ebx,0
+    int 0x80
 
 procedure:
+    push ebx; этот пуш для того, чтобы сохранять все используемые регистры
+    push ecx; тоже самое 
+    push edx;тоже самое
 
-push ebx; этот пуш для того, чтобы сохранять все используемые регистры
-push ecx; тоже самое 
-push edx;тоже самое
+    push ebx
+    mov bh,0
+    and cx,bx
 
+    pop ebx
 
+    mov bl,bh
+    mov bh,0
+    or bx,dx
+    add cx,bx
+    and cx,63
 
-push ebx
+    mov ah,0
+    sub ax,cx
 
-mov bh,0
-
-and cx,bx
-
-
-pop ebx
-
-mov bl,bh
-
-mov bh,0
-
-or bx,dx
-
-add cx,bx
-
-and cx,63
-
-
-mov ah,0
-
-sub ax,cx
-
-
-pop edx
-pop ecx
-pop ebx
-ret
-
-
-
-
-
-
-
+    pop edx
+    pop ecx
+    pop ebx
+    ret
